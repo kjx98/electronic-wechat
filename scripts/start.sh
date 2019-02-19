2d8994cad03ab3e23b408f77330f61a89724d67e
Jesse Kuang <2829915232@qq.com>
2019-02-19 16:44:29 +0800

    fix start.sh 相对路径bug

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/sh
 
 dirn=`dirname $0`
+cd $dirn
+dirn=`pwd`
 tmpf="/tmp/wechat.desktop"
 sed -e "s?WECHATDIR?$dirn?" < $dirn/wechat.desktop > $tmpf
 desq="$HOME/.local/share/applications/wechat.desktop"
